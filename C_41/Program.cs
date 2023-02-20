b74cef597bce5210f66e6a0627a64315414b035e
Sergei Vasiliev <9670105747@mail.ru>
2023-02-20 11:12:08 +0700

    Task 41

--- C_41/Program.cs
+++ C_41/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+// Задача 41: Пользователь вводит с клавиатуры M чисел в одну линию 
+// (не используем Enter). Числа разделены пробелами. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0 7 8 -2 -2 -> 2
+//1 -7 567 89 223-> 4
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+
